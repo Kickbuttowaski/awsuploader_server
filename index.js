e6c1c272ff78734c240ec5d94d83eba19f60e07f
promod96 <pramod@leadfriday.com>
2022-06-25 17:15:44 +0530

    CORS 1) CORS header added as middleware 2) whitelisted client URL

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 const PORT = process.env.port || 4000;
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "PUSH, POST, GET");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 app.get("/",(req,res)=>{
     res.status(200).json({message:"Reached!!!!!"})
 })
